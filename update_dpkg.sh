9943218e36b402b20692dcc203d46d3bf8e35459
Egor Egorov <egigoka@gmail.com>
2022-11-30 19:16:42 +0600

    tested update_dpkg.sh, it works after rejailbreaking' ' '

--- update_dpkg.sh
+++ update_dpkg.sh
@@ -6,9 +6,21 @@ alias frm="dpkg --force-all --remove"
 cp /usr/bin/dpkg /usr/bin/dpkg_bak
 cp /bin/rm /bin/rm_bak
 
-# TODO:
-echo I need to store files somewhere
-exit
+# download dependencies
+DEBS=/var/mobile/procursus_debs_uncompressed
+mkdir $DEBS
+
+wget https://github.com/egigoka/uncursus-revived/blob/master/debs/fakesh_0.0.0.1_iphoneos-arm.deb?raw=true -O $DEBS/fakesh_0.0.0.1_iphoneos-arm.deb
+wget https://github.com/egigoka/uncursus-revived/blob/master/debs/libintl8_0.21-4.0.1_iphoneos-arm.deb?raw=true -O $DEBS/libintl8_0.21-4.0.1_iphoneos-arm.deb
+wget https://github.com/egigoka/uncursus-revived/blob/master/debs/libiosexec1_1.2.2.0.1_iphoneos-arm.deb?raw=true -O $DEBS/libiosexec1_1.2.2.0.1_iphoneos-arm.deb
+wget https://github.com/egigoka/uncursus-revived/blob/master/debs/liblzma5_5.2.5-3.0.1_iphoneos-arm.deb?raw=true -O $DEBS/liblzma5_5.2.5-3.0.1_iphoneos-arm.deb
+wget https://github.com/egigoka/uncursus-revived/blob/master/debs/libz-ng2_2.0.6.0.1_iphoneos-arm.deb?raw=true -O $DEBS/libz-ng2_2.0.6.0.1_iphoneos-arm.deb
+wget https://github.com/egigoka/uncursus-revived/blob/master/debs/libmd0_1.0.4.0.1_iphoneos-arm.deb?raw=true -O $DEBS/libmd0_1.0.4.0.1_iphoneos-arm.deb
+wget https://github.com/egigoka/uncursus-revived/blob/master/debs/liblz4-1_1.9.3.0.1_iphoneos-arm.deb?raw=true -O $DEBS/liblz4-1_1.9.3.0.1_iphoneos-arm.deb
+wget https://github.com/egigoka/uncursus-revived/blob/master/debs/lz4_1.9.3.0.1_iphoneos-arm.deb?raw=true -O $DEBS/lz4_1.9.3.0.1_iphoneos-arm.deb
+wget https://github.com/egigoka/uncursus-revived/blob/master/debs/liblzma5_5.2.5-3.0.1_iphoneos-arm.deb?raw=true -O $DEBS/liblzma5_5.2.5-3.0.1_iphoneos-arm.deb
+wget https://github.com/egigoka/uncursus-revived/blob/master/debs/libzstd1_1.5.2.0.1_iphoneos-arm.deb?raw=true -O $DEBS/libzstd1_1.5.2.0.1_iphoneos-arm.deb
+wget https://github.com/egigoka/uncursus-revived/blob/master/debs/dpkg_1.21.9.0.1_iphoneos-arm.deb?raw=true -O $DEBS/dpkg_1.21.9.0.1_iphoneos-arm.deb
 
 # main script
 frm xz
@@ -17,17 +29,17 @@ frm lz4
 frm xz
 frm lzma
 
-i fakesh_0.0.0.1_iphoneos-arm.deb
-i libintl8_0.21-4.0.1_iphoneos-arm.deb
-i libiosexec1_1.2.2.0.1_iphoneos-arm.deb
-i liblzma5_5.2.5-3.0.1_iphoneos-arm.deb
-i libz-ng2_2.0.6.0.1_iphoneos-arm.deb
-i libmd0_1.0.4.0.1_iphoneos-arm.deb
-i liblz4-1_1.9.3.0.1_iphoneos-arm.deb
-i lz4_1.9.3.0.1_iphoneos-arm.deb
-i liblzma5_5.2.5-3.0.1_iphoneos-arm.deb
-i libzstd1_1.5.2.0.1_iphoneos-arm.deb
-i dpkg_1.21.9.0.1_iphoneos-arm.deb
+i $DEBS/fakesh_0.0.0.1_iphoneos-arm.deb
+i $DEBS/libintl8_0.21-4.0.1_iphoneos-arm.deb
+i $DEBS/libiosexec1_1.2.2.0.1_iphoneos-arm.deb
+i $DEBS/liblzma5_5.2.5-3.0.1_iphoneos-arm.deb
+i $DEBS/libz-ng2_2.0.6.0.1_iphoneos-arm.deb
+i $DEBS/libmd0_1.0.4.0.1_iphoneos-arm.deb
+i $DEBS/liblz4-1_1.9.3.0.1_iphoneos-arm.deb
+i $DEBS/lz4_1.9.3.0.1_iphoneos-arm.deb
+i $DEBS/liblzma5_5.2.5-3.0.1_iphoneos-arm.deb
+i $DEBS/libzstd1_1.5.2.0.1_iphoneos-arm.deb
+i $DEBS/dpkg_1.21.9.0.1_iphoneos-arm.deb
 
 # fuckups
 FILE=/bin/rm
