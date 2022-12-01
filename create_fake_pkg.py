e7b3a78075f92926c99e5d82eb34a52513a6f3de
Egorov Egor <egigoka@gmail.com>
2022-12-01 13:42:49 +0600

    fuck fuck fuck

--- create_fake_pkg.py
+++ create_fake_pkg.py
@@ -4,7 +4,7 @@ pkgs = ['cydia', 'xz', 'lzma', 'libapt-pkg5.0', 'tar']
 
 control_text = """Package: {package_name}
 Essental: yes
-Version: 0.0.0.1
+Version: 99.9.9.9
 Architecture: iphoneos-arm
 Maintainer: Fake pkg <none@example.com>
 Section: System
@@ -16,6 +16,6 @@ Installed-Size: 0
 for pkg in pkgs:
     text = control_text.replace("{package_name}", pkg)
     File.write("./fakepkg/DEBIAN/control", text, mode="w")
-    Console.get_output(["dpkg-deb", "-b", "fakepkg", f"debs/fake{pkg}_0.0.0.1_iphoneos-arm.deb"], print_std=True)
+    Console.get_output(["dpkg-deb", "-b", "fakepkg", f"debs/fake{pkg}_99.9.9.9_iphoneos-arm.deb"], print_std=True)
 
 print("done")
